f9cd042286d3b7c12e28bac95358646744a39388
Sivamani Govindu Nagireddy <nsivamani568@gmail.com>
2025-04-27 08:04:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def save_markdown(company_name, industry_info, company_info, use_cases, datasets):
     with open("output.md", "w") as f:
-        f.write(f"# AI/ML/GenAI Use Cases for {company_name}\n\n")
+        f.write(f"# AI/ML/GenAI Use Cases for {Amazon}\n\n")
         f.write(f"## Industry Info\n{industry_info}\n\n")
         f.write(f"## Company Info\n{company_info}\n\n")
         f.write("## Use Cases\n")
